8e0fa5305ae7c90d68cabf6767c30e5546a179b1
mushahidq <mushahidq@gmail.com>
2021-02-22 20:38:05 +0530

    uses json now

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::io::Read;
-use std::str::FromStr;
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action");
@@ -19,7 +17,7 @@ fn main() {
     } else if action =="complete" {
         match todo.complete(&item) {
             None => println!("'{}' is not present in the list", item),
-            Some(_) => match.todo.save() {
+            Some(_) => match todo.save() {
                 Ok(_) => println!("todo saved"),
                 Err(why) => println!("an error occured: {}", why),
             },
@@ -39,30 +37,31 @@ impl Todo {
         self.map.insert(key, true);
     }
 
-    fn save(self) -> Result<(), std::io::Error> {
-        let mut content = String::new();
-        for (k,v) in self.map {
-            let record = format!("{}\t{}\n", k, v);
-            content.push_str(&record)
-        }
-        std::fs::write("db.txt", content)
+    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        // open db.json
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+        //write to file with serde
+        serde_json::to_writer_pretty(f, &self.map)?;
+        Ok(())
     }
 
     fn new() -> Result<Todo, std::io::Error> {
-        let mut f = std::fs::OpenOptions::new()
+        let f = std::fs::OpenOptions::new()
             .write(true)
             .create(true)
             .read(true)
-            .open("db.txt")?;
-        let mut content = String::new();
-        f.read_to_string(&mut content)?;
-        let map: HashMap<String, bool> = content
-            .lines()
-            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
-            .map(|v| (v[0], v[1]))
-            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
-            .collect();
-        Ok(Todo { map })
+            .open("db.json")?;
+        // serialize json as HashMap
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("an error occured: {}", e),
+        }
     }
 
     fn complete(&mut self, key: &String) -> Option<()> {
